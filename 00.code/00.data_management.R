6727da6d9bc86da5c731b20bfb1a8917340e93fb
Luis-CGH <luis.guevara@uexternado.edu.co>
2026-03-28 15:30:29 -0500

    creates variable lists

--- 00.code/00.data_management.R
+++ 00.code/00.data_management.R
@@ -16,7 +16,7 @@ library(haven) #Loads dta files
 pat <- "https://docs.google.com/spreadsheets/d/%s/export?format=xlsx"
 files <- openxlsx::read.xlsx(sprintf(pat, "16wl4CKW5qfN-qTNEK-UigQOIYr2ixJ69igutvGiJCt4"))
 
-# Loads and names files into a list ----------------------------------------------------------------
+### Loads and names files into a list ----------------------------------------------------------------
 
 #| label: function-googledrive_loader 
 #| Objective: loads .dta or .csv files from google drive based on extension
@@ -51,5 +51,29 @@ lapply(cap_list, function(x) {
 
 names(cap_list) <- files$cap
 
+### Creates a variable list for each chapter -------------------------------------------------------
+  
+var_list <- data.frame(var = NA, cap = NA)
 
+for(i in 1:length(files$cap)){
+  aux <- data.frame(var = names(cap_list[[files$cap[i]]]),cap = files$cap[i] )
+  var_list <- rbind(var_list, aux)
+  rm(aux)
+  var_list <- var_list[is.na(var_list$var) == F,]
+};rm(i)
 
+#setwd(paste0(wd,"/02.aux_tables"));list.files()
+#var_lab <- openxlsx::read.xlsx('2023 04 208 - diccionario III de datos ECSC 2022.xlsx')
+var_lab <- openxlsx::read.xlsx(sprintf(pat, "1S5Arq1UiexCIYFdB_Ldj768D0XFpfMBi"))
+
+var_lab$id <- paste0(var_lab$var,var_lab$var_lab)
+table(duplicated(var_lab$id))
+var_lab <- var_lab[!duplicated(var_lab$id),]
+
+table(var_list$var %in% var_lab$var);table(var_lab$var %in% var_list$var) 
+
+var_list <- merge(var_list,var_lab, all.x = TRUE, by = 'var')
+#openxlsx::write.xlsx(var_list, 'var_list.xlsx')
+
+rm(files_f,var_lab,files)
+openxlsx::write.xlsx(var_list,"01.var_list.xlsx")

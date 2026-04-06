87e639b6e259e8a1bdcbfe10bb0962da227d71dc
Luis-CGH <luiscarlosguevara@beway.com>
2026-04-06 18:02:23 -0500

    Exports varlist

--- 00.code/00.data_management.R
+++ 00.code/00.data_management.R
@@ -62,8 +62,7 @@ for(i in 1:length(files$cap)){
   var_list <- var_list[is.na(var_list$var) == F,]
 };rm(i)
 
-#setwd(paste0(wd,"/02.aux_tables"));list.files()
-#var_lab <- openxlsx::read.xlsx('2023 04 208 - diccionario III de datos ECSC 2022.xlsx')
+### Creates a variable list for each chapter -------------------------------------------------------
 var_lab <- openxlsx::read.xlsx(sprintf(pat, "1S5Arq1UiexCIYFdB_Ldj768D0XFpfMBi"))
 
 var_lab$id <- paste0(var_lab$var,var_lab$var_lab)
@@ -75,4 +74,7 @@ table(var_list$var %in% var_lab$var);table(var_lab$var %in% var_list$var)
 var_list <- merge(var_list,var_lab, all.x = TRUE, by = 'var')
 
 rm(files_f,var_lab,files)
-openxlsx::write.xlsx(var_list,"01.var_list.xlsx")
+# openxlsx::write.xlsx(var_list,"01.var_list.xlsx")
+
+
+
